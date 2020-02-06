00d4091c5e5acbb701157b13f85ecdc1591b7a0c
Athar_Laique <athar@softcircles.org>
2020-02-06 16:15:28 +0500

    Write your own  pipe

--- src/app/custome.pipe.ts
+++ src/app/custome.pipe.ts
@@ -1,4 +1,17 @@
 // Step 0 : create file for writing your own pipe 
 // Step 1 : import interface to write your interface
 import {Pipe , PipeTransform} from '@angular/core';
-//
+// Step 2 : define name of your pipe
+@Pipe({
+    name:'custome'
+})
+// Step 3 :implement pipetransform interface and  export as class
+export class CutomePipe implements PipeTransform {
+ 
+    //implentation 
+    // this function must take a vlue as input and must return something
+      transform(value:any)
+      {
+          return value.substr(0,10)
+      }
+}
